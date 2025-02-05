5b33f5fc31d0c2a37c8e1d4010ceed8a31851de0
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2025-02-05 10:23:55 -0300

    feat: add authorization functions

---
+++ src/utils/auth.js
@@ -0,0 +1,39 @@
+const BASE_URL = "https://se-register-api.en.tripleten-services.com/v1";
+
+function authorize({password, email}) {
+  return fetch(`${BASE_URL}/signin`, {
+    method: POST,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      "password": password,
+      "email": email
+    })
+  })
+}
+
+function register({password, email}) {
+  return fetch(`${BASE_URL}/signin`, {
+    method: POST,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      "password": password,
+      "email": email
+    })
+  })
+}
+
+function checkToken(token) {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: GET,
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization" : `Bearer ${token}`
+    }
+  })
+}
+
+export {register, authorize, checkToken};
