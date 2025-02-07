a9c083ec7bc29a32c6d772fd506a820f06744a4c
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2025-02-07 10:09:55 -0300

    refactor: update Instruction component

--- src/components/Header/components/Instruction/Instruction.jsx
+++ src/components/Header/components/Instruction/Instruction.jsx
@@ -1,8 +1,16 @@
 import '../../../../blocks/header.css';
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 export default function Instruction() {
+  const location = useLocation();
+  let instruction = 'Entrar';
+
+  if(location.pathname == '/signup') {
+    instruction = 'Faça o login';
+  }
+
   return (
-    <Link to="/signin" className='header__instruction'>Entrar</Link>
+    <Link to="/signin" className='header__instruction'>{instruction}</Link>
   )
 }
