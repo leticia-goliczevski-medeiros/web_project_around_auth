20077409383962eb6f0e8379e873f98ddffc5115
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2025-02-06 21:46:48 -0300

    style: style login form

--- src/components/Main/components/Popup/LoginForm/LoginForm.jsx
+++ src/components/Main/components/Popup/LoginForm/LoginForm.jsx
@@ -1,38 +1,39 @@
-import '../../../../../blocks/popup.css'
+import '../../../../../blocks/auth-form.css';
+import { Link } from 'react-router-dom';
 
 export default function LoginForm({onClosePopup}) {
   return (
-    <form name='login-popup'
-        className="popup__form"
-        noValidate>
-      <div className="popup__inputs">
-        <input
-          className="popup__input login-popup__input_email"
-          type="email"
-          placeholder="Email"
-          minLength="2"
-          maxLength="30"
-          required
-        />
-        <span
-          className="popup__input-error login-popup__input_email_error"
-        ></span>
-        <input
-          className="popup__input login-popup__input_password"
-          type="password"
-          placeholder="Senha"
-          required
-        />
-        <span
-          className="popup__input-error login-popup__input_password_error"
-        ></span>
-    </div>
-      <button
-        className="popup__submit-button login-popup__submit-button"
-        type="submit"
-      >
-        Entrar
-      </button>
-    </form>
+    <>
+      <form name='login-form'
+          className="form"
+          noValidate>
+        <h2 className='form__title'>Entrar</h2>
+        <div className="form__inputs">
+          <input
+            className="form__input login-form__input_email"
+            type="email"
+            placeholder="Email"
+            minLength="2"
+            maxLength="30"
+            required
+          />
+          <input
+            className="form__input login-form__input_password"
+            type="password"
+            placeholder="Senha"
+            required
+          />
+      </div>
+        <button
+          className="form__submit-button login-form__submit-button"
+          type="submit"
+        >
+          Entrar
+        </button>
+        <p className='form__text'>Ainda não é membro? <Link to="/register" className='form__link' >Inscreva-se aqui!</Link> </p>
+      </form>
+      
+    </>
+    
   )
 }
