2615ccada9deed64b5d47dd7ecfc0e686204ba0c
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2025-02-20 20:44:30 -0300

    feat: add real-time validation to LoginForm and RegisterForm

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,53 +1,58 @@
 import '../../blocks/auth-form.css';
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { ErrorMessage } from "@hookform/error-message";
 
 export default function LoginForm({handleLogin}) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const { register, handleSubmit, formState: { errors } } = useForm({mode: "onChange"});
 
-  function handleEmailChange(event) {
-    setEmail(event.target.value); 
-  };
-
-  function handlePasswordChange(event) {
-    setPassword(event.target.value); 
-  };
-
-  function handleSubmit(event) {
-    event.preventDefault(); 
-
-    if (!email || !password) {
-      return;
-    } 
-
+  function onSubmit(data) {
+    const {password, email} = data;
     handleLogin({password, email})
   };
 
   return (
-      <form name='login-form'
-        onSubmit={handleSubmit}
-          className="form"
-          noValidate>
+      <form 
+        name='login-form'
+        onSubmit={handleSubmit(onSubmit)}
+        className="form"
+        noValidate>
         <h2 className='form__title'>Entrar</h2>
         <div className="form__inputs">
           <input
-            value={email}
-            onChange={handleEmailChange}
+            name="email"
+            { ...register('email', {required: "Esse campo é obrigatório.", minLength: {
+              value: 2,
+              message: "O email precisa ter no mínimo 2 caracteres."
+            }, maxLength: {
+              value: 40,
+              message: "O email precisa ter no máximo 40 caracteres."
+            }, pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: 'Formato de email inválido.'
+            }}) }
             className="form__input login-form__input_email"
             type="email"
             placeholder="Email"
-            minLength="2"
-            required
           />
+
+          <ErrorMessage errors={errors} name="email" render={({ message }) => <p className='form__input-error form__input-error_email'>{message}</p>} />
+
           <input
-            value={password}
-            onChange={handlePasswordChange}
+            name="password"
+            { ...register('password', {required: "Esse campo é obrigatório.", minLength: {
+              value: 2,
+              message: "A senha precisa ter no mínimo 2 caracteres."
+            }, maxLength: {
+              value: 40,
+              message: "A senha precisa ter no máximo 40 caracteres."
+            }}) } 
             className="form__input login-form__input_password"
             type="password"
             placeholder="Senha"
-            required
           />
+
+          <ErrorMessage errors={errors} name="password" render={({ message }) => <p className='form__input-error form__input-error_password'>{message}</p>} />
       </div>
         <button
           className="form__submit-button login-form__submit-button"
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,59 +1,65 @@
 import '../../blocks/auth-form.css';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { useState, useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { ErrorMessage } from "@hookform/error-message";
+
 import Popup from '../Main/components/Popup/Popup.jsx';
+
 import { PopupContext } from '../../contexts/PopupContext.js';
 
 export default function RegisterForm({handleRegisterUser}) {
   const { popup } = useContext(PopupContext);
+  const { register, handleSubmit, formState: { errors } } = useForm({mode: "onChange"});
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('')
-
-  function handleEmailChange(event) {
-    setEmail(event.target.value);
-  }
-
-  function handlePasswordChange(event) {
-    setPassword(event.target.value);
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    if (!email || !password) {
-      return;
-    } 
-
+  function onSubmit(data) {
+    const {password, email} = data;
     handleRegisterUser({password, email})
   }
 
   return (
     <>
       <form 
-          onSubmit={handleSubmit}
+          onSubmit={handleSubmit(onSubmit)}
           name='register-form'
           className="form"
           noValidate>
         <h2 className='form__title'>Inscrever-se</h2>
         <div className="form__inputs">
           <input
-            value={email}
-            onChange={handleEmailChange}
+            name="email"
+            { ...register('email', {required: "Esse campo é obrigatório.", minLength: {
+              value: 2,
+              message: "O email precisa ter no mínimo 2 caracteres."
+            }, maxLength: {
+              value: 40,
+              message: "O email precisa ter no máximo 40 caracteres."
+            }, pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: 'Formato de email inválido.'
+            }}) }
             className="form__input register-form__input_email"
             type="email"
             placeholder="Email"
-            minLength="2"
-            required
           />
+
+          <ErrorMessage errors={errors} name="email" render={({ message }) => <p className='form__input-error form__input-error_email'>{message}</p>} />
+
           <input
-            value={password}
-            onChange={handlePasswordChange}
+            name="password"
+            { ...register('password', {required: "Esse campo é obrigatório.", minLength: {
+              value: 2,
+              message: "A senha precisa ter no mínimo 2 caracteres."
+            }, maxLength: {
+              value: 40,
+              message: "A senha precisa ter no máximo 40 caracteres."
+            }}) } 
             className="form__input register-form__input_password"
             type="password"
             placeholder="Senha"
-            required
           />
+
+          <ErrorMessage errors={errors} name="password" render={({ message }) => <p className='form__input-error form__input-error_password'>{message}</p>} />
       </div>
         <button
           className="form__submit-button register-form__submit-button"
